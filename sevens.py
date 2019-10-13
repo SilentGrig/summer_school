2c4162778a89969195d95d85428bee1ac24525be
Greg Thomas <silentgrig@github.com>
2019-10-13 13:50:54 +0100

    finish sevens game

--- sevens.py
+++ sevens.py
@@ -1,10 +1,13 @@
 import cardGame
 
 layouts = {"D": [], "C": [], "H": [], "S": []}
-AceHigh = False
+ACE_HIGH = False
 
 
 def getNumberOfPlayers():
+    """ Gets number of player's from user.
+        Number of players must be at least 3
+        Returns an int """
     numberOfPlayers = int(input("Please enter the number of players: "))
     while not numberOfPlayers >= 3:
         numberOfPlayers = int(input(
@@ -13,90 +16,173 @@ def getNumberOfPlayers():
 
 
 def getStartingPlayer(hands):
-    startingPlayer = cardGame.getHandIndexOfCard(hands, "7S")
+    """ Determines which player starts.
+        Starting player has Seven of Spades.
+        Returns int, -1 if card not found """
+    startingPlayer = cardGame.getHandIndexOfCard(hands, "S7")
     if startingPlayer is None:
         return -1
     return startingPlayer
 
 
 def getNextPlayer(currentPlayer, numberOfPlayers):
+    """ Gets next player, loops back to first player
+        when after last player has been reached
+        Returns int """
     return (currentPlayer + 1) % numberOfPlayers
 
 
 def isGameOver(hands):
+    """ Determines if game is over.
+        Game is over when one player's hand is empty """
+    for hand in hands:
+        if len(hand) == 0:
+            return True
     return False
 
 
 def printLayouts():
+    """ Prints layouts (card's played) to the screen
+        Doesn't Return """
     for suit in layouts.keys():
         print(suit + " - " + str(layouts[suit]))
 
 
 def getPlayerCommand():
-    command = input("Enter card to play or type 'pass' to pass: ")
-    while not command == "pass":
-        command = input("Enter card to play or type 'pass' to pass: ")
+    """ Get's an input from the user
+        Returns String """
+    command = input("Enter card to play, 'pass' or 'quit': ")
     return command
 
 
 def getValidCards():
+    """ Gets all the valid cards that could be played
+        Returns list Strings """
     validCards = []
     for suit in layouts.keys():
         playedCards = layouts[suit]
         if len(playedCards) == 0:
-            validCards.append("7" + suit)
+            validCards.append(suit + "7")
         else:
-            validCards.append(getNextLowerAndUpperCard(suit, playedCards))
+            validCards += getNextLowerAndUpperCard(suit, playedCards)
+    return validCards
 
 
 def getNextLowerAndUpperCard(suit, cards):
+    """ Gets the next lower and highest card.
+        Assumes cards are sorted from lowest to highest value
+        Return list """
+    if len(cards) == 0:
+        return None
     cardsToReturn = []
     lowestCard = cards[0]
     highestCard = cards[-1]
-    lowestCardValue = int(cardGame.convertCardToValue(
-        lowestCard, AceHigh
-        )[:2])
+    lowestCardValue = cardGame.getCardValue(lowestCard, ACE_HIGH)[1:]
+    # don't add a lower card if one doesn't exist
     if lowestCardValue > 1:
-        cardsToReturn.append(cardGame.convertCardToRank(
-            str(lowestCardValue - 1) + suit)
+        cardsToReturn.append(
+            cardGame.convertCardToRank(suit + str(lowestCardValue - 1))
         )
-    highestCardValue = int(cardGame.convertCardToValue(
-        highestCard, AceHigh)[:2])
-    if highestCardValue < 12:
-        cardsToReturn.append(cardGame.convertCardToRank(
-            str(highestCardValue + 1) + suit)
+    highestCardValue = cardGame.getCardValue(highestCard, ACE_HIGH)
+    # don't add a higher card if one doesn't exist
+    if highestCardValue < 13:
+        cardsToReturn.append(
+            cardGame.convertCardToRank(suit + str(highestCardValue + 1))
         )
     return cardsToReturn
 
 
 def isValidCommand(command, hand):
+    """ Determines if command given is valid
+        Checks if player has a valid card that could be played
+        Returns Boolean """
+    if command == "quit":
+        return True
+
     validCards = getValidCards()
+    validMoves = list(filter(lambda card: card in validCards, hand))
+
+    if len(validMoves) == 0 and command == "pass":
+        return True
+
+    return command in validMoves
+
+
+def sortHands(hands):
+    """ Sort each hand
+        Doesn't Return """
+    for hand in hands:
+        cardGame.sortHandByRank(hand)
+
+
+def getWinningPlayer(hands):
+    """ Gets the winning player, index of empty hand plus 1
+        First player is player 1
+        Returns int """
+    for index, hand in enumerate(hands):
+        if len(hand) == 0:
+            return index + 1
+    return -1
+
+
+def updateHandAndLayouts(pickedCard, hand):
+    """ Removes card from hand and adds to the layouts in sorted order
+        Assumes picked card is a valid card
+        Doesn't Return """
+    hand.remove(pickedCard)
+    valueCard = cardGame.convertCardToValue(pickedCard, ACE_HIGH)
+    suit = valueCard[0]
+    value = int(valueCard[1:])
+    if value < 7:
+        layouts[suit].insert(0, pickedCard)
+    else:
+        layouts[suit].append(pickedCard)
 
 
 def initialSetUp():
+    """ Perform initial setup for game
+        Returns a tuple of starting variables
+        deck, hands, numberOfPlayers, startingPlayer """
     deck = cardGame.getDeck()
     cardGame.shuffleDeck(deck)
     numberOfPlayers = getNumberOfPlayers()
     hands = cardGame.dealCards(deck, 0, numberOfPlayers)
+    sortHands(hands)
     startingPlayer = getStartingPlayer(hands)
     return deck, hands, numberOfPlayers, startingPlayer
 
 
 def main():
+    """ Main Game Function """
     deck, hands, numberOfPlayers, currentPlayer = initialSetUp()
 
     while True:
-        print("It is " + str(currentPlayer + 1) + "'s turn:")
+        print()
+        print("Player " + str(currentPlayer + 1) + "'s turn:")
         currentPlayerHand = hands[currentPlayer]
         print("Your hand is - " + str(currentPlayerHand))
         printLayouts()
         command = getPlayerCommand()
 
-        if not command == "pass":
-            pass
+        while not isValidCommand(command, currentPlayerHand):
+            print("Invalid Command!")
+            print()
+            print("Player " + str(currentPlayer + 1) + "'s turn:")
+            print("Your hand is - " + str(currentPlayerHand))
+            printLayouts()
+            command = getPlayerCommand()
 
-        if isGameOver(hands):
+        if command not in ["pass", "quit"]:
+            updateHandAndLayouts(command, currentPlayerHand)
+
+        if isGameOver(hands) or command == "quit":
             break
+
+        currentPlayer = getNextPlayer(currentPlayer, numberOfPlayers)
+
+    if isGameOver(hands):
+        print("Player " + str(getWinningPlayer(hands)) + " wins!")
+    print("Goodbye")
 
 if __name__ == "__main__":
     main()
--- test_sevens.py
+++ test_sevens.py
@@ -3,8 +3,8 @@ import sevens
 
 def testCorrectStartingPlayerReturned():
     hands = []
-    playerOneHand = ["3H", "7S", "KC"]
-    playerTwoHand = ["AS", "7C", "QD"]
+    playerOneHand = ["H3", "S7", "CK"]
+    playerTwoHand = ["SA", "C7", "DQ"]
     hands.append(playerOneHand)
     hands.append(playerTwoHand)
     startingPlayer = sevens.getStartingPlayer(hands)
@@ -13,27 +13,68 @@ def testCorrectStartingPlayerReturned():
 
 def testNoPlayerReturnedWhenNoSevenOfSpades():
     hands = []
-    playerOneHand = ["3H", "7D", "KC"]
-    playerTwoHand = ["AS", "7C", "QD"]
+    playerOneHand = ["H3", "D7", "CK"]
+    playerTwoHand = ["SA", "C7", "DQ"]
     hands.append(playerOneHand)
     hands.append(playerTwoHand)
     startingPlayer = sevens.getStartingPlayer(hands)
     assert(startingPlayer == -1)
 
 
-def getNextPlayerReturnsNextPlayer():
+def testGetNextPlayerReturnsNextPlayer():
     currentPlayer = 0
     numberOfPlayers = 4
     assert(sevens.getNextPlayer(currentPlayer, numberOfPlayers) == 1)
 
 
-def getNextPlayerReturnsIndexZeroAfterLastPlayer():
+def testGetNextPlayerReturnsIndexZeroAfterLastPlayer():
     currentPlayer = 2
     numberOfPlayers = 3
     assert(sevens.getNextPlayer(currentPlayer, numberOfPlayers) == 0)
 
 
+def testGetNextLowerAndUpperCardsEmptyList():
+    suit = "D"
+    cards = []
+    nextCards = sevens.getNextLowerAndUpperCard(suit, cards)
+    assert(nextCards is None)
+
+
+def testGetNextLowerAndUpperCardsSingleCard():
+    suit = "S"
+    cards = ["S7"]
+    nextCards = sevens.getNextLowerAndUpperCard(suit, cards)
+    assert(nextCards == ["S6", "S8"])
+
+
+def testGetNextLowerAndUpperCardsLowerBoundary():
+    suit = "H"
+    cards = ["HA", "H2", "H3", "H4", "H5", "H6", "H7"]
+    nextCards = sevens.getNextLowerAndUpperCard(suit, cards)
+    assert(nextCards == ["H8"])
+
+
+def testGetNextLowerAndUpperCardsHigherBoundary():
+    suit = "H"
+    cards = ["H7", "H8", "H9", "H10", "HJ", "HQ", "HK"]
+    nextCards = sevens.getNextLowerAndUpperCard(suit, cards)
+    assert(nextCards == ["H6"])
+
+
+def testGetValidCards():
+    validCards = sevens.getValidCards()
+    filterValidCards = filter(
+        lambda card: card in ["H7", "S7", "D7", "C7"], validCards
+        )
+    assert(len(list(filterValidCards)) == len(validCards))
+
+
 testCorrectStartingPlayerReturned()
 testNoPlayerReturnedWhenNoSevenOfSpades()
-getNextPlayerReturnsNextPlayer()
-getNextPlayerReturnsIndexZeroAfterLastPlayer()
+testGetNextPlayerReturnsNextPlayer()
+testGetNextPlayerReturnsIndexZeroAfterLastPlayer()
+testGetNextLowerAndUpperCardsEmptyList()
+testGetNextLowerAndUpperCardsSingleCard()
+testGetNextLowerAndUpperCardsLowerBoundary()
+testGetNextLowerAndUpperCardsHigherBoundary()
+testGetValidCards()
